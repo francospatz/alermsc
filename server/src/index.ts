996df007ca936c0db73ba8af6c36c1aec3422d4e
Alejandro de la Orden Collado <alex@makekites.io>
2024-08-27 21:05:53 +0200

    [ADD] Server start

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response, urlencoded } from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); // This way .env variables are available anywhere in the project.
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+if (process.env.ENV === "dev") {
+  (async () => {
+    const morgan = (await import("morgan")).default; // Dinamic import for morgan, so it's only used if we are under the 'development' environment.
+    app.use(morgan("dev"));
+  })();
+}
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Aleja's server API!");
+});
+
+app.listen(port, () => {
+  console.log(`[SERVER]: Server is running at port ${port}`);
+});
